fd644873f85066da7097f1ca2d255cc6cdbc4747
Nick Cadiente <nickcadiente@Nicks-MacBook-Pro.local>
2015-12-16 10:58:14 -1000

    works by brute force needs to have no protect

--- challenge_7.js
+++ challenge_7.js
@@ -1,1 +1,31 @@
 // do work here
+
+function PrimeFinder(n) {
+  this.number = n;
+  this.nthPrime = null;
+}
+function isPrime(number){
+    for(var i=2; i<number; i++){
+      if (number%i===0){
+        return false;
+      }
+    }
+    return true;
+  }
+  PrimeFinder.prototype.getPrimeNumber = function () {
+    var primeCounter = 0;
+    var number = this.number;
+    var currentNumber = 2;
+    while (primeCounter !== number) {
+      if(isPrime(currentNumber)) {
+        primeCounter++;
+        currentNumber++;
+        } else {
+        currentNumber++;
+      }
+    }
+    return currentNumber - 1;
+  };
+
+  var thousandthOne = new PrimeFinder(6);
+  console.log(thousandthOne.getPrimeNumber());
